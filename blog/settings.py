76407fa876617f3f065ad95f4f2973b8d0e8ba21
Sonatkbenny <139038607+Sonatkbenny@users.noreply.github.com>
2025-03-24 20:52:48 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xux@aa_=2!paq(tl5td%myg*#cunx&6apfp5_emm02r560a6xt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blogdjango-gcanhwg5cqgbdgh6.centralus-01.azurewebsites.net','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['miniblog-e0hmbug5ezdnc7fw.centralus-01.azurewebsites.net','127.0.0.1','localhost']
 
 
 # Application definition
